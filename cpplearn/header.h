7d6ddd8355ac87e34785842ced844479dbd87bbe
Zhong Xingjian <zxjzxj0010@126.com>
2024-06-26 20:56:14 +0800

    new cpp learn

---
+++ cpplearn/header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+using namespace std;
+class Solution
+{
+    public:
+        void print();
+    private:
+        int a = 10;
+};      
+#endif
